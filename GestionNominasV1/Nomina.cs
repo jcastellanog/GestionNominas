6ba593a40a107018730f7a00cdae5e28337ade94
jcastellanog <jesus.castellano@hotmail.com>
2024-01-16 09:32:33 +0100

    Creación de la Propiedad NombreEmpleado

--- GestionNominasV1/Nomina.cs
+++ GestionNominasV1/Nomina.cs
@@ -16,5 +16,27 @@ namespace GestionNominasV1
         private int _horasTrabajadas;
         private float _salarioHora;
 
+        // PROPIEDADES
+        /// <summary>
+        /// Nombre del Empleado
+        /// </summary>
+        public string NombreEmpleado
+        {
+            get {
+                // Control de Contenido
+                if (string.IsNullOrEmpty(_nombre)) throw new Exception("Nombre del Trabajador no establecido");
+
+                // Devolución del Valor
+                return _nombre;
+            }
+            set {
+                // Validación de los Datos a Establecer
+                if (string.IsNullOrEmpty(value)) throw new Exception("El Nombre del Trabajador no puede ser null o la cadena vacía");
+
+                // Establecimiento del valor al miembro privado
+                _nombre = value;
+            }
+        }
+
     }
 }
