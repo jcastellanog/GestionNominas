d89a26200f318367ec635c684039d4cc7844e1ab
jcastellanog <jesus.castellano@hotmail.com>
2024-01-16 09:14:23 +0100

    Adición de Miembros Privados a la Clase

--- GestionNominasV1/Nomina.cs
+++ GestionNominasV1/Nomina.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,12 @@ namespace GestionNominasV1
 {
     public class Nomina
     {
+        // MIEMBROS PRIVADOS DE LA CLASE
+        private string _nombre;
+        private string _apellidos;
+        private string _puesto;
+        private int _horasTrabajadas;
+        private float _salarioHora;
+
     }
 }
