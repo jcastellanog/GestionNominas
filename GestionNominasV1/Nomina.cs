01bbd75977a4d77271f893ef923227a6327c0a1a
jcastellanog <jesus.castellano@hotmail.com>
2024-01-16 08:52:57 +0100

    Agregar archivos de proyecto.

---
+++ GestionNominasV1/Nomina.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionNominasV1
+{
+    public class Nomina
+    {
+    }
+}
---
+++ GestionNominasV1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GestionNominasV1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Controlador Principal
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // CONSTANTES
+
+            // RECURSOS
+
+            // INICIALIZACIÓN DE RECURSOS
+
+            // ENTRADA
+
+            // PROCESO
+
+            // SALIDA
+        }
+    }
+}
