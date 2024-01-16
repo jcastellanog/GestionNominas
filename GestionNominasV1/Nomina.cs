74a0493521572d8a67f913a3fe36ce61e63e9ccb
jcastellanog <jesus.castellano@hotmail.com>
2024-01-16 20:18:41 +0100

    Reversión de cambios

--- GestionNominasV1/Nomina.cs
+++ GestionNominasV1/Nomina.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,44 @@ namespace GestionNominasV1
 {
     public class Nomina
     {
+        // MIEMBROS PRIVADOS DE LA CLASE
+        private string _nombre;
+        private string _apellidos;
+        private string _puesto;
+        private int _horasTrabajadas;
+        private float _salarioHora;
+
+        // PROPIEDADES
+        /// <summary>
+        /// Nombre del Empleado
+        /// </summary>
+        public string NombreEmpleado
+        {
+            get {
+                // Control de Contenido
+                if (string.IsNullOrEmpty(_nombre)) throw new Exception("Nombre del Trabajador no establecido");
+
+                // Devolución del Valor
+                return _nombre;
+            }
+            set {
+                // Validación de los Datos a Establecer
+                if (string.IsNullOrEmpty(value)) throw new Exception("El Nombre del Trabajador no puede ser null o la cadena vacía");
+
+                // Establecimiento del valor al miembro privado
+                _nombre = value;
+            }
+        }
+
+        public string ApellidosEmpleado{
+            get{
+
+                return _apellidos;
+            }
+            set{
+                _apellidos = value;
+            }
+        }
+
     }
 }
